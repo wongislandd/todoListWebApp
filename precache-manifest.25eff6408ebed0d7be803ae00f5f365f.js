fb914f3bdd07dd84e2b56f60c679e06336c9b9f6
Christopher Wong <46093907+cwong51799@users.noreply.github.com>
2019-12-05 12:35:53 -0500

    Updates

---
+++ precache-manifest.25eff6408ebed0d7be803ae00f5f365f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf833d5900aead627ca5cc17677521b1",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "43d7d8eed6b6416dc58b",
+    "url": "/todolist/static/css/main.af803b40.chunk.css"
+  },
+  {
+    "revision": "0eca6132a6fac5067d64",
+    "url": "/todolist/static/js/2.e31434f5.chunk.js"
+  },
+  {
+    "revision": "43d7d8eed6b6416dc58b",
+    "url": "/todolist/static/js/main.39056c8a.chunk.js"
+  },
+  {
+    "revision": "cb035873d9ef3230c873",
+    "url": "/todolist/static/js/runtime-main.ba964518.js"
+  }
+]);
